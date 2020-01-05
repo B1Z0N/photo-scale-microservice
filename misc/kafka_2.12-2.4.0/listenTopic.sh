a367d9bc3b7d839c2face7159be80d5867c7a0ce
B1Z0N <kolausf@gmail.com>
2020-01-05 17:24:52 +0200

    Added support for kafka shortcuts

--- misc/kafka_2.12-2.4.0/listenTopic.sh
+++ misc/kafka_2.12-2.4.0/listenTopic.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic "$1"
+if (( $# > 1 )); then
+    bin/kafka-console-consumer.sh --bootstrap-server localhost:19092 --topic "$2"
+else
+    bin/kafka-console-consumer.sh --bootstrap-server 10.50.5.1:9092 --topic "$1"
+fi
--- misc/kafka_2.12-2.4.0/send2Topic.sh
+++ misc/kafka_2.12-2.4.0/send2Topic.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-bin/kafka-console-producer.sh --broker-list localhost:9092 --topic "$1"
+if (( $# > 1 )); then
+    bin/kafka-console-producer.sh --broker-list localhost:19092 --topic "$2"
+else
+    bin/kafka-console-producer.sh --broker-list 10.50.5.1:9092 --topic "$1"
+fi
